85120d026d1cb13bdccfbeba774674d17dab9e14
ucd-os-chartreuse-s18 <matthew.moltzau@ucdenver.edu>
2018-03-23 18:29:23 -0600

    Trying to use the hash table. Not working now.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -207,7 +207,7 @@ thread_create (const char *name, int priority,
   t = palloc_get_page (PAL_ZERO);
   if (t == NULL)
     return TID_ERROR;
-    
+  
   /* Initialize thread. */
   init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
@@ -235,8 +235,17 @@ thread_create (const char *name, int priority,
   sf->eip = switch_entry;
   sf->ebp = 0;
   
-  struct thread *tc = thread_current();
-  list_push_back (&tc->children_list, &t->child_elem);
+  /*
+  //Migrating this to process currently
+  printf ("About to insert.\n");
+  struct thread *tc = thread_current ();
+  printf ("%s is executing\n", tc->name);
+  //if (tc == NULL) printf ("tc is NULL\n");
+  if (t == NULL) printf ("t is NULL\n");
+  //if (&tc->children_hash == NULL) printf ("hash is NULL");
+  //hash_insert (&tc->children_hash, &t->hash_elem);
+  printf ("Done inserting.\n");
+  */
   
   /* Add to run queue. */
   thread_unblock (t);
@@ -677,7 +686,9 @@ init_thread (struct thread *t, const char *name, int priority)
   list_init (&t->donators);
   sema_init (&t->thread_sema, 0);
   
-  list_init (&t->children_list);
+  //Can't initialize hash table here, see start_process.
+  //keyed_hash_init (&t->children_hash);
+  
   sema_init (&t->dying_sema, 0);
   sema_init (&t->status_sema, 0);
   
